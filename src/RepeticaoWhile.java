1cd1bf58562de6fb8ce1d75b0422be93224fb660
Joacil Amarante <joacil.amarante@gmail.com>
2022-10-03 11:29:54 -0400

    .

---
+++ src/RepeticaoWhile.java
@@ -0,0 +1,12 @@
+public class RepeticaoWhile {
+    public static void main(String[] args) {
+
+        int numero = 0;
+
+        while(numero <= 10){
+            System.out.println("numero: " + numero);
+            numero++;
+        }
+
+    }
+}
