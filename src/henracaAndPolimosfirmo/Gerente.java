a5a2807aa03b741fa12e65c874293e1ff51a7534
Joacil Amarante <joacil.amarante@gmail.com>
2022-10-11 10:07:19 -0400

    .

---
+++ src/henracaAndPolimosfirmo/Gerente.java
@@ -0,0 +1,34 @@
+package henracaAndPolimosfirmo;
+
+public class Gerente extends Funcionario {
+
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public double getBonificacao() {
+        return super.getBonificacao();
+    }
+
+    //    public double getBonificacao(){
+//        //super = o atribulto salario é da classe pai
+////        return ( this.salario * 0.1) + super.salario;
+//        return super.salario;
+//    }
+
+
+}
+
+
+
---
+++ src/henracaAndPolimosfirmo/TesteGerente.java
@@ -0,0 +1,23 @@
+package henracaAndPolimosfirmo;
+
+public class TesteGerente {
+
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Joacil");
+        g1.setCpf("333.333.333-33");
+        g1.setSalario(5000.0);
+
+
+        System.out.println(g1.getNome() + " | " + g1.getCpf() + " | " + g1.getSalario() + g1.getBonificacao());
+
+        g1.setSenha(222);
+        boolean autentica = g1.autentica(222);
+        System.out.println(autentica);
+
+
+
+
+
+    }
+}
